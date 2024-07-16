b3684dfc999e7ff6f4aa354829767686947fddc7
Guy Mesika <guy@coti.io>
2024-07-16 21:08:07 +0300

    version 0.4.5 supporting 0.5.5 SDK

---
+++ src/util/general-utils.ts
@@ -0,0 +1,23 @@
+import {ContractTransactionReceipt, Provider, Wallet} from "ethers";
+import fs from "fs";
+
+export function getWallet(provider: Provider) {
+    if (!process.env.SIGNING_KEY) {
+        const wallet = Wallet.createRandom(provider)
+
+        setEnvValue("SIGNING_KEY", `${wallet.privateKey}`)
+        console.log("************* Created new account ", wallet.address, " and saved into .env file *************")
+
+        throw new Error(`Please use faucet to fund account ${wallet.address}`)
+    }
+
+    return new Wallet(process.env.SIGNING_KEY, provider)
+}
+
+export function setEnvValue(key: string, value: string) {
+    fs.appendFileSync("./.env", `\n${key}=${value}`, "utf8")
+}
+
+export function validateTxStatus(tx: any) {
+    return tx?.status === 1
+}
